71bbbf0f3443635cff777315b206dda2ae0c8e3a
5Stygian <121655781+Blitzy5656@users.noreply.github.com>
2024-10-17 13:55:33 -0700

    Create summation.cpp

---
+++ summation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sum() {
+    // inputs
+    cout << "starting value (must be larger than 0): ";
+    cin >> ii; // index input
+    cout << "upper limit: ";
+    cin >> upperLimit;
+    cout << "equation (must use 'ii'): ";
+    cin >> equation;
+
+    // functionality
+    for (int upperLimit < upperLimit + 1; ii++) {
+        output = equation
+        cout << "=======\n" << output "=======\n"
+    }
+}
